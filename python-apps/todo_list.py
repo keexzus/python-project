ac076a6b5da93b5a21bd657162588c5578c20be3
Enrique Avalos <keex@Enriques-Air.lan>
2023-07-15 19:08:18 -0700

    refresh

--- python-apps/todo_list.py
+++ python-apps/todo_list.py
@@ -1,8 +1,17 @@
 user_prompt = "Enter a todo: "
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
 
-todos = [todo1, todo2, todo3]
-print(todos)
-print(type(todos))
+# instead of repeating code as below, have while loop
+# while 2 > 1 :
+while True:
+    todo = input(user_prompt)
+    print(todo)
+    print("Type next todo item... ")
+
+
+# todo1 = input(user_prompt)
+# todo2 = input(user_prompt)
+# todo3 = input(user_prompt)
+
+# todos = [todo1, todo2, todo3]
+# print(todos)
+# print(type(todos))
