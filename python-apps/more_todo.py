d45c4d9c6044001e86722be8dda8035d8a8b84b5
Enrique Avalos <keex@Enriques-Air.lan>
2023-07-16 18:05:16 -0700

    more todo exercises

---
+++ python-apps/more_todo.py
@@ -0,0 +1,15 @@
+
+user_prompt = "Enter a todo: "
+
+todos = [ ]
+
+while True:
+    todo = input(user_prompt)
+    todos.append(todo)
+    
+    print(todos)
+
+# while True:
+#     todo = input(user_prompt)
+#     todos = [todo]
+#     print(todos)
