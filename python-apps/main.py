125f77d949e5dbcd6064a52b43bbf1f631fb33a3
Enrique Avalos <keex@Enriques-MacBook-Air.local>
2023-07-20 11:20:40 -0700

    application update

--- python-apps/main.py
+++ python-apps/main.py
@@ -3,7 +3,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, or exit: ")
+    user_action = input("Type add, show, edit, complete, or exit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -12,13 +12,16 @@ while True:
             todos.append(todo)
         case 'show' | 'display':
             for index, item in enumerate(todos):
-                row = f"{index}-{item}"
+                row = f"{index + 1}-{item}"
                 print(row)
         case 'edit':
             number = int(input("Number of the todo to edit: "))
             number = number - 1
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo 
+        case 'complete':
+            number = int(input("Number of of task to delete: "))
+            todos.pop(number -1 )
         case 'exit':
             break
         case _:
