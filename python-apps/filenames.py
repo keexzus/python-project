ff364215c379298dc9105704ce29b47874463da1
Enrique Avalos <keex@Enriques-MacBook-Air.local>
2023-07-20 09:49:30 -0700

    for loops

---
+++ python-apps/filenames.py
@@ -0,0 +1,8 @@
+filenames = ["1.Raw Data.txt", "2.Reports.txt", "3.Presentations.txt"]
+
+for filenames in filenames:
+    filenames = filenames.replace('.', '-', 1)
+    print(filenames)
+
+    # //for real world use// 
+    # file.rename(filenames)
---
+++ python-apps/one_liners.py
@@ -0,0 +1,21 @@
+# a = input("Enter any input: ")
+
+# print(a * 10)
+
+# hi * 5 = hihihihihi
+mylist = [ 'a', 'b' ]
+x = 10.1
+y = 10
+
+mylist.__setitem__(0, 28)
+print(mylist[0])
+
+filename = "1.Raw Data.text"
+# print(filename.replace('.', '-'))
+
+filename_new = filename.replace('.', '-', 1)
+print(filename_new)
+ 
+
+# print(myList.index('b'))
+# print(dir(list))
