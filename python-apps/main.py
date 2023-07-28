093351b3c434ca503057713cae5cb7d3c3068169
Enrique Avalos <keex@Enriques-Air.lan>
2023-07-28 14:28:53 -0700

    refactored

--- python-apps/main.py
+++ python-apps/main.py
@@ -1,6 +1,5 @@
 # user_prompt = "Enter a todo: "
-
-# todos = []
+todos = []
 
 while True:
     user_action = input("Type add, show, edit, complete, or exit: ")
@@ -10,31 +9,47 @@ while True:
         case 'add':
             todo = input("Enter an action: ") + "\n"
             
-            file = open('/todo.txt', 'r')
-            todos = file.readlines()
-            file.close()
+            # file = open('text-todo.txt', 'r')
+            # todos = file.readlines()
+            # file.close()
+
+            with open('text-todo.txt', 'r') as file:
+                todos = file.readlines()
 
             todos.append(todo)
 
-            file = open('/todo.txt', 'w')
-            file.writelines(todos)
-            file.close()
+            with open('text-todo.txt', 'w') as file:
+                file.writelines(todos)
+
+            # file = open('text-todo.txt', 'w')
+            # file.writelines(todos)
+            # file.close()
 
         case 'show' | 'display':
-            file = open('/todo.txt', 'r')
+            file = open('text-todo.txt', 'r')
             todos = file.readlines()
             file.close()
+
+            new_todos = []
+
+            # for item in todos:
+            #     new_item = item.strip('\n')
+            #     new_todos.append(new_item)
+            # print(todos)
+            new_todos = [item.strip('\n') for item in todos]
+            
             
             for index, item in enumerate(todos):
-                row = f"{index + 1}-{item}"
-                print(row)
+                row = f"{index + 1}-{item.strip('')}"
+                print(row, end = '')
                 # print("length is", index + 1)
                 # print((len(todos)))
         case 'edit':
             number = int(input("Number of the todo to edit: "))
             number = number - 1
             new_todo = input("Enter new todo: ")
-            todos[number] = new_todo 
+            todos[number] = new_todo
+
         case 'complete':
             number = int(input("Number of of task to delete: "))
             todos.pop(number -1 )
